2fecc68830697593d3974fccf29d18fe5e13a77e
Lyman Gillispie <l@lygi.me>
2015-07-23 13:51:41 -0400

    19: added test for gemrc

--- devops/spec/localhost/local_developer_env_spec.rb
+++ devops/spec/localhost/local_developer_env_spec.rb
@@ -13,3 +13,7 @@ describe command('git config --list'), :if => os[:family] == 'darwin' do
 	its(:stdout) { should contain('user.email') }
 end
 
+describe file(ENV["HOME"] + '/.gemrc'), :if => os[:family] == 'darwin' do
+	it { should exist }
+        it { should contain 'gem: --user-install'}
+end
