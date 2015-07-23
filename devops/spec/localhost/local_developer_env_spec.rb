31e8dc8b9711b8024d61a85a3a092236054c1c73
Josef Salyer <jsalyer@pillartechnology.com>
2015-07-23 13:30:23 -0400

    19: verified git is set up correctly

--- devops/spec/localhost/local_developer_env_spec.rb
+++ devops/spec/localhost/local_developer_env_spec.rb
@@ -4,3 +4,12 @@ describe file('/usr/bin/xcodebuild'), :if => os[:family] == 'darwin' do
   it { should exist }
 end
 
+describe file('/usr/bin/git'), :if => os[:family] == 'darwin' do
+	it { should exist }
+end
+
+describe command('git config --list'), :if => os[:family] == 'darwin' do
+	its(:stdout) { should contain('user.name') }
+	its(:stdout) { should contain('user.email') }
+end
+
