64689df9361817c3c586bddd4838c8751781ba61
msomji <maisam.somji@gmail.com>
2015-07-23 13:58:14 -0400

    19: added test for ssh key

--- devops/spec/localhost/local_developer_env_spec.rb
+++ devops/spec/localhost/local_developer_env_spec.rb
@@ -1,15 +1,25 @@
 require 'spec_helper'
+require 'rspec/expectations'
 
-describe file('/usr/bin/xcodebuild'), :if => os[:family] == 'darwin' do
-  it { should exist }
+$whoami = `whoami`.chomp!
+
+
+describe "local environment" do
+  describe file('/usr/bin/xcodebuild'), :if => os[:family] == 'darwin' do
+    it { should exist }
+  end
+
+  describe file('/usr/bin/git'), :if => os[:family] == 'darwin' do
+    it { should exist }
+  end
+
+  describe command('git config --list'), :if => os[:family] == 'darwin' do
+    its(:stdout) { should contain('user.name') }
+    its(:stdout) { should contain('user.email') }
+  end
+
+  describe file("/Users/#{$whoami}/.ssh/id_rsa.pub"), :if => os[:family] == 'darwin' do
+    it { should exist }
+  end
+
 end
-
-describe file('/usr/bin/git'), :if => os[:family] == 'darwin' do
-	it { should exist }
-end
-
-describe command('git config --list'), :if => os[:family] == 'darwin' do
-	its(:stdout) { should contain('user.name') }
-	its(:stdout) { should contain('user.email') }
-end
-
