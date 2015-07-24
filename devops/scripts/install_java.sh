41167b5102bec3378fb8eaa8175367d482819c9c
Josef Salyer <jsalyer@pillartechnology.com>
2015-07-24 09:31:48 -0400

    19 added converted mono doc, cleaning up java tests and files

--- devops/scripts/install_java.sh
+++ devops/scripts/install_java.sh
@@ -1,9 +1,81 @@
 #! /usr/bin/env bash
 # this is for Java 1.8
 
-echo 'installing java'
+TOOL='java'
+
+
+#commands
+CURL=`which curl`
+HDIUTIL=`which hdiutil`
+INSTALLER=`which installer`
+RM=`which rm`
+JAVA=`which java`
 
 URL="http://javadl.sun.com/webapps/download/AutoDL?BundleId=108140"
+PATH='/tmp/jre-8u51-macosx-x64.dmg'
+VOLUME="/Volumes/Java 8 Update 51"
+PACKAGE="${VOLUME}/Java 8 Update 51.pkg"
 
 
-curl -L $URL > '/tmp/jre-8u51-macosx-x64.dmg'
+
+download_java() {
+	echo 'downloading java'
+	$CURL -L $URL > $PATH
+	exit
+}
+
+mount_installer() {
+	echo 'mounting dmg'
+	$HDIUTIL attach $PATH
+	exit
+}
+
+install_java() {
+	echo 'installing java'
+	${INSTALLER} -pkg "${PACKAGE}" -target /
+	exit
+}
+
+
+uninstall_java() {
+	echo 'uninstalling java'
+	$RM -fr "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin"
+	$RM -fr /Library/PreferencePanes/JavaControlPanel.prefpane
+	exit
+}
+
+cleanup(){
+	echo 'removing temp files'
+	$RM -rf $PATH
+}
+
+unmount() {
+	echo 'unmounting dmg'
+	$HDIUTIL detach "${VOLUME}"
+	exit
+}
+
+usage() {
+	echo "./scripts/install_java.sh download|mount|install|unmount|uninstall"
+	exit
+}
+
+
+case $1 in
+	'download' )
+		download_java ;;
+	'mount' )
+		mount_installer ;;
+	'install' )
+		install_java ;;
+	'unmount' )
+		unmount ;;
+	'uninstall' )
+		uninstall_java ;;
+	'cleanup' )
+		cleanup ;;
+	*) 
+		usage;;
+esac
+
+
---
+++ mono_how_to_get_started.md
@@ -0,0 +1,17 @@
+1)  Download and install monodevelop for mac os x
+
+2)  Download and install mono mdk (link when starting monodevelop)
+
+3)  New solution -&gt; .Net -&gt; Console Project
+
+4)  Right click “Packages” -&gt; “Add Package”
+
+5)  Search “Nancey.Hosting.Self” and add
+
+6)  Edit “Program.cs” and add the following in the Main method:\
+    ![](media/image1.tiff)
+
+7)  Create a class names “LlamaModule.cs” with:\
+    ![](media/image2.tiff)
+
+8)  Build and debug, open browser to “http://localhost:8080”
