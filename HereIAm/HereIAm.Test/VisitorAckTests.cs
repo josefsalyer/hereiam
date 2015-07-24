35e09fa25df71923ac8d85906d14ede5012f133e
James Stumme <jstumme@pillartechnology.com>
2015-07-24 14:46:47 -0400

    Setup VisitorAckTests test class.

---
+++ HereIAm/HereIAm.Test/VisitorAckTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using Nancy;
+using Nancy.Testing;
+
+namespace HereIAm.Test
+{
+	[TestFixture]
+	public class VisitorAckTests
+	{
+		private Browser _client;
+
+		[SetUp]
+		public void InitializeTest()
+		{
+			// Arrange
+			_client = new Browser (new DefaultNancyBootstrapper ());
+		}
+
+
+	}
+}
+
