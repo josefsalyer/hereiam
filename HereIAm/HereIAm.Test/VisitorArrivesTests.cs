424b256c2c5750e9720633c3706c98efe40d63ea
James Stumme <jstumme@pillartechnology.com>
2015-07-24 11:28:24 -0400

    Passed UserArrivesWihValidPhoneNumberPostReturnsOK test.

---
+++ HereIAm/HereIAm.Test/VisitorArrivesTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using Nancy;
+using Nancy.Testing;
+
+namespace HereIAm.Test
+{
+	[TestFixture ()]
+	public class VisitorArrivesTests
+	{
+		// Name | Phone    | Results
+		// josef|5551234567|accepted
+		[Test ()]
+		public void UserArrivesWihValidPhoneNumberPostReturnsOK ()
+		{
+			// Arrange
+			var client = new Browser (new DefaultNancyBootstrapper ());
+
+			// Act
+			var results = client.Post ("/arrival/5551234567", x => {
+				x.HttpRequest ();
+			});
+
+			// Assert
+			Assert.AreEqual (HttpStatusCode.OK, results.StatusCode);
+		}
+	}
+}
+
---
+++ HereIAm/HereIAm/ArrivalModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nancy;
+
+namespace HereIAm
+{
+	public class ArrivalModule : NancyModule
+	{
+		public ArrivalModule () : base("/arrival")
+		{
+			Post ["/{phoneNumber}"] = param => {
+				return HttpStatusCode.OK;
+			};
+		}
+	}
+}
+
