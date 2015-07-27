a44bcd58b843d5976a62e788b84b5b1e144f3dae
Josef Salyer <jsalyer@pillartechnology.com>
2015-07-27 14:54:42 -0400

    up and running on ubuntu

--- devops/spec/default/ubuntu_mono_spec.rb
+++ devops/spec/default/ubuntu_mono_spec.rb
@@ -4,7 +4,45 @@ describe package('nginx'), :if => os[:family] == 'ubuntu' do
   it { should be_installed }
 end
 
+describe file('/var/www/hereiam') do
+  it { should exist }
+  it { should be_directory }
+end 
 
-describe port(80) do
+# add the nginx config to available sites
+describe file('/etc/nginx/sites-available/hereiam') do
+  it { should exist }
+  it { should be_file }
+end 
+
+
+
+
+#setup mono environmnet
+describe package('mono-devel'), :if => os[:family] == 'ubuntu' do
+	it { should be_installed}
+end
+
+describe package('mono-complete'), :if => os[:family] == 'ubuntu' do
+	it { should be_installed}
+end
+
+describe command('mono --version'), :if => os[:family] == 'ubuntu' do
+	its(:stdout) { should contain('Mono JIT compiler version 4.0.2') }
+end
+
+
+#compile the app and run it
+
+# start the app
+describe port(4567), :if => os[:family] == 'ubuntu' do
   it { should be_listening }
 end
+
+
+
+# start nginx
+describe port(80), :if => os[:family] == 'ubuntu' do
+  it { should be_listening }
+end
+
