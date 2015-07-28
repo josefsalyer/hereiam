3739766545895067b759f93a55f5cb2515853c30
Josef Salyer <jsalyer@pillartechnology.com>
2015-07-28 14:20:21 -0400

    34: added initial DAL class and layer.  all tests pass

---
+++ HereIAm/HereIAm.DAL/DBConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Raven.Client.Document;
+
+namespace HereIAm.DAL
+{
+	public class DBConnection
+	{
+		public DocumentStore Store {
+			get;
+			set;
+		}
+
+		public DBConnection ()
+		{
+			this.Store = new DocumentStore ();
+		}
+	}
+}
+
---
+++ HereIAm/HereIAm.Test/DBConnectionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using HereIAm.DAL;
+using Raven.Client.Document;
+
+namespace HereIAm.Test
+{
+	[TestFixture]
+	public class DBConnectionTests
+	{
+		[Test]
+		public void TestDBConnectionNotNull ()
+		{
+			var db = new DBConnection ();
+			Assert.NotNull (db);
+		}
+
+		[Test]
+		public void TestReferenceToRavenDB()
+		{
+			var store = new DocumentStore ();
+			Assert.NotNull (store);
+		}
+
+		[Test]
+		public void TestDBConnectionHasDocumentStore()
+		{
+			var db = new DBConnection ();
+			Assert.NotNull (db.Store);
+		}
+	}
+}
+
