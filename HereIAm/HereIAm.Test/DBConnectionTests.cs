a12c6b057c13949b6dc6b127e6599d1366d74271
Josef Salyer <jsalyer@pillartechnology.com>
2015-07-28 15:36:23 -0400

    34: test for DB connection initialization

--- HereIAm/HereIAm.Test/DBConnectionTests.cs
+++ HereIAm/HereIAm.Test/DBConnectionTests.cs
@@ -30,14 +30,45 @@ namespace HereIAm.Test
 		}
 
 		[Test]
-		public void TestStoreIsConfigured ()
+		public void TestStoreUrlIsConfigured ()
 		{
 			
 			var db = new DBConnection ();
 			var actual = db.Store.Url;
 			var expected = @"https://lrtconsulting-5noh.ravenhq.com/databases/lrtconsulting-hereiam";
 			Assert.AreEqual (expected, actual);
+
 		}
+
+		[Test]
+		public void TestStoreApiKeyIsConfigured ()
+		{
+
+			var db = new DBConnection ();
+			var actual = db.Store.ApiKey;
+			var expected = @"9db681e5-51c1-491a-b8ea-9dd8f12e09e0";
+			Assert.AreEqual (expected, actual);
+
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void TestStoreIsInitialized()
+		{	
+			var db = new DBConnection ();
+			Assert.Throws (typeof (System.InvalidOperationException), new TestDelegate (DelegateMethod(db)));
+
+			db.Store.Initialize ();
+
+			Assert.DoesNotThrow (new TestDelegate (DelegateMethod(db)));
+		}
+
+		dynamic DelegateMethod(DBConnection dbconn){
+			
+			return dbconn.Store.DatabaseCommands;
+
+		}
+
 	}
 }
 
