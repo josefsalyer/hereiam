4f47cfaead5ec671f852594163764ef147d635ef
Lyman Gillispie <l@lygi.me>
2015-07-28 16:50:38 -0400

    24: added person tests

---
+++ HereIAm/HereIAm.Test/PersonTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using HereIAm.Dto;
+
+namespace HereIAm.Test
+{
+	[TestFixture]
+	public class PersonTests
+	{
+		[Test]
+		public void PersonClassExists ()
+		{
+			Person actual = new Person ();
+			Assert.IsInstanceOf<Person> (actual);
+		}
+
+		[Test]
+		public void PersonHasANameAndAPhoneNumber ()
+		{
+			var expectedName = "Maisam";
+			var expectedPhoneNumber = "1234567890";
+			Person actual = new Person (expectedName, expectedPhoneNumber);
+
+			Assert.AreEqual (actual.Name, expectedName);
+			Assert.AreEqual(actual.PhoneNumber, expectedPhoneNumber);
+		}
+			
+	}
+
+}
+
