ba99c09c8f24d0e91dc8169c27f742acd7f86d24
Lyman Gillispie <l@lygi.me>
2015-07-28 17:22:03 -0400

    24: Events can be created with a single host.

--- HereIAm/HereIAm.Dto/Event.cs
+++ HereIAm/HereIAm.Dto/Event.cs
@@ -1,11 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HereIAm.Dto
 {
 	public class Event
 	{
+
+		public List<Person> Hosts {
+			get;
+			set;
+		}
+
+		public List<Person> Visitors {
+			get;
+			set;
+		}
+
 		public Event ()
 		{
+			Hosts =	new List<Person>();
+			Visitors =	new List<Person>();
+		}
+
+		public Event (Person host) : this()
+		{
+			this.Hosts.Add (host);
 		}
 	}
 }
--- HereIAm/HereIAm.Test/EventTests.cs
+++ HereIAm/HereIAm.Test/EventTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
 using HereIAm.Dto;
+using System.Collections.Generic;
 
 namespace HereIAm.Test
 {
@@ -12,6 +13,30 @@ namespace HereIAm.Test
 		{
 			Event actual = new Event ();
 			Assert.IsInstanceOf<Event> (actual);
+		}
+
+		[Test]
+		public void EventClassHasAListOfHosts ()
+		{
+			Event actual = new Event ();
+			Assert.IsInstanceOf<List<Person>> (actual.Hosts);
+		}
+
+		[Test]
+		public void EventClassHasAListOfVisitors ()
+		{
+			Event actual = new Event ();
+			Assert.IsInstanceOf<List<Person>> (actual.Visitors);
+		}
+
+		[Test]
+		public void EventClassHasOneHostOnInitialization()
+		{
+			Person expectedHost = new Person ();
+			expectedHost.Name = "Joe";
+			expectedHost.PhoneNumber = "1234567890";
+			Event actual = new Event (expectedHost);
+			Assert.AreEqual (expectedHost, actual.Hosts [0]);
 		}
 	}
 }
