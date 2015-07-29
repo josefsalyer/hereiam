598efc11f7ae83ec0a2d58d7b8ff3ba1655b9672
James Stumme <jstumme@pillartechnology.com>
2015-07-29 13:49:48 -0400

    Passed ThrowsExceptionWhenInvalidNumberEntered test.

---
+++ HereIAm/HereIAm.Test/PhoneNumberTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace HereIAm.Test
+{
+	[TestFixture]
+	public class PhoneNumberTests
+	{
+		[Test]
+		[ExpectedException(typeof(ArgumentException), ExpectedMessage = "Bad phone number")]
+		public void ThrowsExceptionWhenInvalidNumberEntered()
+		{
+			//Act
+			new PhoneNumber("BADBEEF123");
+		}
+	}
+}
---
+++ HereIAm/HereIAm/PhoneNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace HereIAm
+{
+	public class PhoneNumber
+	{
+		private static readonly Regex _phonePattern = 
+			new Regex (@"^\(?\d{3}\)?-? ?\d{3}-? ?-?\d{4}$");
+
+		private string _phoneNumber;
+
+		public PhoneNumber (string phoneNumber)
+		{
+			if (phoneNumber == null)
+				throw new ArgumentNullException ("phoneNumber");
+
+			if (!_phonePattern.IsMatch (phoneNumber))
+				throw new ArgumentException ("Bad phone number");
+				
+			_phoneNumber = phoneNumber;
+		}
+	}
+}
+
