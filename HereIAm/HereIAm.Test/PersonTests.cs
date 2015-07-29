86a1844269121e8c8b7960942d131475c267e3b3
Lyman Gillispie <l@lygi.me>
2015-07-29 15:37:05 -0400

    24: Added person class test to auto generate ID

--- HereIAm/HereIAm.Test/PersonTests.cs
+++ HereIAm/HereIAm.Test/PersonTests.cs
@@ -13,6 +13,24 @@ namespace HereIAm.Test
 			Person actual = new Person ();
 			Assert.IsInstanceOf<Person> (actual);
 		}
+
+		[Test]
+		public void CanBeCreatedWithExistingAttributes()
+		{
+			var expectedName = "Mack the Knife";
+			var expectedNumber = "1232341234";
+			var expectedGuid = Guid.NewGuid ().ToString ();
+			Person actual = new Person (expectedName, expectedNumber, expectedGuid);
+			Assert.AreEqual (expectedGuid, actual.Id);
+			Assert.AreEqual (expectedNumber, actual.PhoneNumber);
+			Assert.AreEqual (expectedName, actual.Name);	
+		}
+
+		[Test]
+		public void GenerateGUIDWhenOneIsntThere () {
+			Person actual = new Person ("Kimmy the Hammer", "9075552727");
+			Assert.IsNotNull (actual.Id);
+		}
 	}
 
 }
